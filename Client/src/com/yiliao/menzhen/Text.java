3dc9ed771cdcb8a54856a9a7aadb5d541fabcdac
“zhuwei290” <2903293094@qq.com>
2024-04-19 09:43:28 +0800

    验证码类

--- Client/src/com/yiliao/menzhen/Text.java
+++ Client/src/com/yiliao/menzhen/Text.java
@@ -1,8 +1,10 @@
 package com.yiliao.menzhen;
 
+import java.util.Optional;
+
 public class Text {
     public static void main(String[] args) {
-        
-
+        String str=CheckCodeUtil.generateVerifyCode(5);
+        System.out.println(str);
     }
 }
